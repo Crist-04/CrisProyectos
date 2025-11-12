3fa47267ebf7662c522f5f233275e6c76f283fe1
digis <digis@192.167.1.105>
2025-11-12 16:30:16 -0600

    Ya mapea datos de Usuario pero no Direccion ni Rol

--- src/main/java/com/digis01/CAlvarezProgramacionNCapasOctubre2025/Configuration/MapperConfig.java
+++ src/main/java/com/digis01/CAlvarezProgramacionNCapasOctubre2025/Configuration/MapperConfig.java
@@ -3,6 +3,9 @@ package com.digis01.CAlvarezProgramacionNCapasOctubre2025.Configuration;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import com.digis01.CAlvarezProgramacionNCapasOctubre2025.JPA.UsuarioJPA;
+import com.digis01.CAlvarezProgramacionNCapasOctubre2025.ML.Usuario;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,12 +14,27 @@ public class MapperConfig {
     
     @Bean
     public ModelMapper modelMapper(){
-        ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.LOOSE)
-                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PUBLIC);
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.addMappings(new PropertyMap<UsuarioJPA, Usuario>() {
+            @Override
+            protected void configure() {
+                map().setIdUsuario(source.IdUsuario);
+                map().setNombre(source.Nombre);
+                map().setUserName(source.Username);
+                map().setApellidoPaterno(source.ApellidoPaterno);
+                map().setApellidoMaterno(source.ApellidoMaterno);
+                map().setEmail(source.Email);
+                map().setPassword(source.Password);
+                map().setFechaNacimiento(source.FechaNacimiento);
+                map().setSexo(source.Sexo);
+                map().setTelefono(source.Telefono);
+                map().setCelular(source.Celular);
+                map().setCURP(source.CURP);
+                
+            }
+        });
         
-        return mapper;
+        return modelMapper;
     }
     
 }
